169ba84d662e982575ad22769deed25c2a5f0979
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 07:59:36 -0500

    Create A6.py

---
+++ A6.py
@@ -0,0 +1,11 @@
+grade = int(input("Enter your grade: "))
+if grade < 60:
+  print('F')
+elif grade < 70:
+  print('D')
+elif grade < 80:
+  print('C')
+elif grade < 90:
+  print('B')
+elif grade >= 90:
+  print('A')
