d5b86b8b1d182f0c51e824efc3d0822df119b8d7
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 08:32:20 -0500

    Create C20.js

---
+++ C20.js
@@ -0,0 +1,15 @@
+    if(a==b){
+        if(a==c){
+            return(20);
+        }else{
+            return(10);
+        }
+    }else if(a==c){
+        return(10);
+    }else if(b==c){
+        return(10);
+    }else{
+        return(0);
+    }
+}
+greenTicket(1,2,3);
