2e981b51c2c69929223e805c688d13849b2124a9
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 08:15:01 -0500

    Create A37.py

---
+++ A37.py
@@ -0,0 +1,14 @@
+#check how to read and open a file from A20
+a=open('alice.txt','r')
+
+#create a function to find the longest word
+def longestWord(file_name,):
+    max=''
+    count=0
+    b=list(a)
+    print(b)
+    for words in b:
+        print(words)
+        for letter in words:
+            count+=1
+            return letter
