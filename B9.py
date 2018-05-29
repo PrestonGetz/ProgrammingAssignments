f32117b185ac02e96155284ec586548400edd409
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 08:22:17 -0500

    Create B9.py

---
+++ B9.py
@@ -0,0 +1,18 @@
+# Don't delete any of the given code.
+def maxBlock(strings):
+    # insert your logic here
+
+# Test cases. Don't modify  
+print(1,maxBlock("fez day"))   # this would be 2
+print(2,countYZ("day fez"))   # this would be 3
+print(3,maxBlock("hoopla"))	  # this would be 0
+print(4,maxBlock("abbCCCddBBBxx"))	
+print(5,maxBlock(""))	
+print(6,maxBlock("xyz"))	
+print(7,maxBlock("xxyz"))	
+print(8,maxBlock("xyzz"))	
+print(9,maxBlock("abbbcbbbxbbbx"))	
+print(10,maxBlock("XXBBBbbxx"))	
+print(11,maxBlock("XXBBBBbbxx"))	
+print(12,maxBlock("XXBBBbbxxXXXX"))
+print(12,maxBlock("XX2222BBBbbXX2222")
