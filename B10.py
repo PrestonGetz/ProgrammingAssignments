f6b4b7302ea58dba8c8e67763ba94b2392305544
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 08:22:41 -0500

    Create B10.py

---
+++ B10.py
@@ -0,0 +1,15 @@
+# Don't delete any of the given code.
+def numberFun(a, b, c):
+  # insert your logic here
+  
+  
+# Test cases. Don't modify  
+print(1,numberFun(1, 2, 3))	     # Possible
+print(2,numberList(4, 5, 1))	   	 # Possible
+print(3,numberList(10, 2, 4))	     # Impossible	
+print(4,numberList(9, 2, 18))	     
+print(5,numberList(9, 5, 14))	   	
+print(6,numberList(90, 25, 65))	   
+print(7,numberList(288, 16, 18))
+print(8,numberList(56, 5, 260))
+print(9,numberList(4, 65, 260))
