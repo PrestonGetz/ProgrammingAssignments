a6036377c8ea6b99141254e821d791107e6fadc5
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 08:25:42 -0500

    Create C4.js

---
+++ C4.js
@@ -0,0 +1,8 @@
+function isEvenAndGreaterThanTen(num) 
+{
+ // your code here
+ if (num > 10 && num%2 === 0){return true;}
+ else{return false;}
+}
+var output = isEvenAndGreaterThanTen(13);
+console.log(output);
