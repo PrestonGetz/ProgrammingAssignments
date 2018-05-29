e5b68967b92614c78982190ca766c7c5ba801b6f
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 08:25:22 -0500

    Create C3.js

---
+++ C3.js
@@ -0,0 +1,10 @@
+function checkAge(name, age) {
+  // your code here
+  if(age<21){
+      console.log("Go home, "+name+"!");
+  }
+  if(age>=21){
+      console.log("Welcome, "+name+"!");
+  }  
+}
+checkAge("Adrian",22);
