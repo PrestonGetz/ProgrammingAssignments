0a2ffa80cbf4ac159fb2734ee907a2969ea563d4
PrestonGetz <39733745+PrestonGetz@users.noreply.github.com>
2018-05-29 08:26:36 -0500

    Create C6.js

---
+++ C6.js
@@ -0,0 +1,10 @@
+function computeSummationToN(n) {
+    // your code here
+    output=0;
+    count=1;
+    for (var i=0;i<(n+1);i++){
+        output+=i;
+    }
+    return(output);
+}
+computeSummationToN(4);
